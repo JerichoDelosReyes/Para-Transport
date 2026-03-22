29300a13def4763a4d572f2802647866847b10ee
Jericho Delos Reyes <jericho.dlsreyes@gmail.com>
2026-03-22 14:51:18 +0800

    chore: remove temporary AI patch and fix scripts

--- color_check.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-// We need a simple PNG parser to get the top left pixel color
-// Let's just output base64 of a 1x1 area if possible, or use a tool.
-// Actually, I don't have a png reader readily available.
