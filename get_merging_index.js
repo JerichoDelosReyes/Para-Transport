c16a7d5cab60417c8fb646913c94d0e80fc91284
Jericho Delos Reyes <jericho.dlsreyes@gmail.com>
2026-03-31 21:11:48 +0800

    chore: remove unnecessary patch and temporary files

--- get_merging_index.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const out = execSync('git show merging:app/(tabs)/index.tsx').toString();
-fs.writeFileSync('merging_index.tsx', out);
